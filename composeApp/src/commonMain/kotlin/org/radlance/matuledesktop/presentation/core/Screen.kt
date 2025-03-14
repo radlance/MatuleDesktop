7f66aa4f2161d32790d2db697fa574258f0c7160
Dmitry <manyakindima@gmail.com>
2025-03-14 18:09:13 +0300

    replace enum with interface

--- composeApp/src/commonMain/kotlin/org/radlance/matuledesktop/presentation/core/Screen.kt
+++ composeApp/src/commonMain/kotlin/org/radlance/matuledesktop/presentation/core/Screen.kt
@@ -1,6 +1,27 @@
 package org.radlance.matuledesktop.presentation.core
 
-enum class Screen {
-    SignIn,
-    SignUp
+import androidx.compose.runtime.Composable
+import org.radlance.matuledesktop.presentation.auth.signin.SignInScreen
+import org.radlance.matuledesktop.presentation.auth.signup.SignUpScreen
+
+interface Screen {
+
+    @Composable
+    fun Show(navigate: (Screen) -> Unit)
+
+    object SignIn : Screen {
+
+        @Composable
+        override fun Show(navigate: (Screen) -> Unit) {
+            SignInScreen(navigateToSignUp = { navigate(SignUp) })
+        }
+    }
+
+    object SignUp : Screen {
+
+        @Composable
+        override fun Show(navigate: (Screen) -> Unit) {
+            SignUpScreen(navigateToSignInScreen = { navigate(SignIn) })
+        }
+    }
 }
--- composeApp/src/commonMain/kotlin/org/radlance/matuledesktop/presentation/core/main.kt
+++ composeApp/src/commonMain/kotlin/org/radlance/matuledesktop/presentation/core/main.kt
@@ -8,12 +8,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import org.radlance.matuledesktop.presentation.auth.signin.SignInScreen
-import org.radlance.matuledesktop.presentation.auth.signup.SignUpScreen
 import java.awt.Dimension
 
 fun main() = application {
-    var currentScreen by remember { mutableStateOf(Screen.SignIn) }
+    var currentScreen by remember { mutableStateOf<Screen>(Screen.SignIn) }
+
     Window(
         title = "MatuleDesktop",
         state = rememberWindowState(width = 800.dp, height = 600.dp),
@@ -21,17 +20,6 @@ fun main() = application {
     ) {
         window.minimumSize = Dimension(350, 600)
 
-
-        when (currentScreen) {
-            Screen.SignIn -> SignInScreen(
-                navigateToSignUp = { currentScreen = Screen.SignUp }
-            )
-
-            Screen.SignUp -> SignUpScreen(
-                navigateToSignInScreen = { currentScreen = Screen.SignIn }
-            )
-        }
-
-
+        currentScreen.Show(navigate = { currentScreen = it })
     }
 }
