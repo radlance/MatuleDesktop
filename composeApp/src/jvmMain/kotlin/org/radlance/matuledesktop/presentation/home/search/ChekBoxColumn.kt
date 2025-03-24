9016c8cae900a9a51ba6896ea5d3a7eb91227e5d
Dmitry <manyakindima@gmail.com>
2025-03-24 10:49:04 +0300

    add color preview on search settings

--- composeApp/src/jvmMain/kotlin/org/radlance/matuledesktop/presentation/home/search/ChekBoxColumn.kt
+++ composeApp/src/jvmMain/kotlin/org/radlance/matuledesktop/presentation/home/search/ChekBoxColumn.kt
@@ -1,15 +1,20 @@
 package org.radlance.matuledesktop.presentation.home.search
 
 import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
@@ -19,11 +24,14 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.unit.dp
 import matuledesktop.composeapp.generated.resources.Res
 import matuledesktop.composeapp.generated.resources.all
 import org.jetbrains.compose.resources.stringResource
+import org.radlance.matuledesktop.domain.product.ProductColor
 
 @Composable
 fun <T> CheckboxGroup(
@@ -62,9 +70,37 @@ fun <T> CheckboxGroup(
         }
 
         Box(modifier = Modifier.height(150.dp)) {
-            Column(modifier = Modifier.fillMaxHeight().verticalScroll(scrollState)) {
+            Column(
+                modifier = Modifier.fillMaxHeight().verticalScroll(scrollState),
+                verticalArrangement = Arrangement.Center
+            ) {
                 items.forEachIndexed { index, item ->
                     Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.width(180.dp)) {
+
+                        (item as? ProductColor)?.let { color ->
+                            Column {
+                                Spacer(Modifier.height(4.dp))
+                                Box(
+                                    modifier = Modifier
+                                        .size(18.dp)
+                                        .clip(CircleShape)
+                                        .border(
+                                            width = 1.dp,
+                                            color = Color.Black,
+                                            shape = CircleShape,
+                                        )
+                                        .background(
+                                            Color(
+                                                red = color.red,
+                                                green = color.green,
+                                                blue = color.blue
+                                            )
+                                        )
+                                )
+                            }
+                            Spacer(Modifier.width(8.dp))
+                        }
+
                         Text(text = itemLabel(item), modifier = Modifier.weight(1f))
                         Checkbox(
                             checked = childCheckedStates[index],
